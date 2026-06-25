41da44e457bb4946064810027a8d252fec52522b
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-06-25 14:05:18 +0630

    feat: add changes

---
+++ AutomatedTesting.Api/Models/Tasks/CreateTaskRequestModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomatedTesting.Api.Models.Tasks;
+
+public class CreateTaskRequestModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
---
+++ AutomatedTesting.Api/Repositories/Tasks/TaskRepository.cs
@@ -0,0 +1,29 @@
+﻿using AutomatedTesting.Api.Models.Tasks;
+using AutomatedTesting.Db.AppDbContextModels;
+
+namespace AutomatedTesting.Api.Repositories.Tasks;
+
+public class TaskRepository : ITaskRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public TaskRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<int> CreateTask(CreateTaskRequestModel request, CancellationToken ct)
+    {
+        var item = new TaskItem
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Iscompleted = false,
+            CreatedAt = DateTime.UtcNow,
+        };
+
+        await _appDbContext.AddAsync(item);
+        var result = await _appDbContext.SaveChangesAsync(ct);
+        return result;
+    }
+}
